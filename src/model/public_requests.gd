45facd1cd1a6db157d524cebc24264dbc3236199
woynert <woynertgamer@gmail.com>
2024-01-27 11:27:29 -0500

    refactor: improve random trick selection

--- src/model/public_requests.gd
+++ src/model/public_requests.gd
@@ -12,9 +12,17 @@ const possible_requests = [
 
 static func generate_request ():
 	# TODO: take in account the day and difficulty and experience
-	var request_pick = possible_requests.pick_random()
+	
+	var generate_easy = [true, false].pick_random()
+	var generate_medium = [true, false].pick_random()
+	var generate_hard = [true, false].pick_random()
+	var forceful = 0
+	if !generate_easy && !generate_medium && !generate_hard:
+		forceful = [1,2,3].pick_random()
+	
 	var request: Array[int] = []
-	for r in request_pick:
-		request.append(r)
+	if generate_easy || forceful == 1: request.append(TRICKS.HANDEABLE.pick_random())
+	if generate_medium || forceful == 2: request.append(TRICKS.MOUNTABLE.pick_random())
+	if generate_hard || forceful == 3: request.append(TRICKS.ON_SITE.pick_random())
+	
 	GlobalState.set_current_request(request)
-
