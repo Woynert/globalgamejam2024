072135ecc14767c93c8a67848aa0ed47df2060cd
woynert <woynertgamer@gmail.com>
2024-01-29 14:13:27 -0500

    fix: play correct music on gameover/win screens

--- src/ui/ui_message/ui_gameover.gd
+++ src/ui/ui_message/ui_gameover.gd
@@ -4,16 +4,25 @@ class_name UiGameOver
 enum OPTION {
 	TRAIN,
 	GAMEOVER,
+	POLO,
 }
 
 const OPTION_IMAGE = {
 	OPTION.TRAIN: "res://resource_remote/Bad ending train tracks.png",
 	OPTION.GAMEOVER: "res://resource_remote/Game Over  Screen.png",
+	OPTION.POLO: "res://resource_remote/Final Poledance.png",
 }
 
 func _ready():
 	$MarginContainer/Button.pressed.connect(_on_return)
-	$TextureRect.texture = load(OPTION_IMAGE[[OPTION.TRAIN, OPTION.GAMEOVER].pick_random()])
+	var option = [OPTION.TRAIN, OPTION.GAMEOVER].pick_random()
+	$TextureRect.texture = load(OPTION_IMAGE[option])
+	
+	match option:
+		OPTION.TRAIN, OPTION.GAMEOVER:
+			SharedRes.get_manager_sound().play_stream_loop(ManagerSound.AUDIO.LOOP_BAD_ENDING)
+		OPTION.POLO:
+			SharedRes.get_manager_sound().play_stream_loop(ManagerSound.AUDIO.LOOP_BAD_ENDING_POLO)
 
 func _on_return():
 	SharedRes.get_manager_events().event_goto_main_menu()
--- src/ui/ui_message/ui_win.gd
+++ src/ui/ui_message/ui_win.gd
@@ -3,19 +3,20 @@ class_name UiWin
 
 enum OPTION {
 	MICKY,
-	BAR,
 	BEACH,
 }
 
 const OPTION_IMAGE = {
 	OPTION.MICKY: "res://resource_remote/Mickey End.png",
-	OPTION.BAR: "res://resource_remote/Final Poledance.png",
 	OPTION.BEACH: "res://resource_remote/Good Ending Retirement.png",
 }
 
 func _ready():
 	$MarginContainer/Button.pressed.connect(_on_return)
-	$TextureRect.texture = load(OPTION_IMAGE[[OPTION.MICKY, OPTION.BAR, OPTION.BEACH].pick_random()])
+	var option = [OPTION.MICKY, OPTION.BEACH].pick_random()
+	$TextureRect.texture = load(OPTION_IMAGE[option])
+	
+	SharedRes.get_manager_sound().play_stream_loop(ManagerSound.AUDIO.LOOP_GOOD_ENDING)
 
 func _on_return():
 	SharedRes.get_manager_events().event_goto_main_menu()
